1d297ba02bc32ecefd84eae1cbb164ef7e523149
Mick T <mick.timony+github.com@gmail.com>
2024-06-19 11:55:51 -0400

    need to specify the subnet_id to resolve the "No default VPC for this user" error

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  subnet_id     = "subnet-7262a858"
 
   tags = {
     Name = "HelloWorld"
